8229258ae34e6c4691805b297b7364a2ce128d58
Tyler Burford <tburford13@gmail.com>
2021-11-07 17:44:15 -0600

    Added basic log in

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,10 +1,46 @@
 import './Landing.css'
+import { Grid, TextField, Button } from '@material-ui/core';
 
 
 function Landing(){
     return (
         <div>
-            Landing page.
+            <Grid container
+              direction = "column"
+              alignItems="center"
+              justify="center"
+              style={{ minHeight: "100vh"}}
+            >
+                <Login />
+            </Grid>
+        </div>
+    );
+}
+
+const Login = () => {
+    return (
+        <div>
+            <Grid container
+                  direction = "column"
+                  alignItems="center"
+                  justify="center"
+                  style={{ minHeight: "100vh"}}
+            >
+                <TextField
+                    margin={"normal"}
+                    id="outlined-basic"
+                    label="Username"
+                    variant="outlined" />
+                <TextField
+                    margin={"normal"}
+                    id="outlined-basic"
+                    label="Password"
+                    variant="outlined" />
+                <Button size={"large"} variant={"contained"} color={"primary"}>
+                    Log in.
+                </Button>
+                Landing page.
+            </Grid>
         </div>
     );
 }
