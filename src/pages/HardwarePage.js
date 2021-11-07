782cf03b21f80b458799dc0f3be9baac9816fd87
tbert93 <tony.bertolino@utexas.edu>
2021-11-07 15:08:01 -0600

    Added hardware frontend and backend communication

--- src/pages/HardwarePage.js
+++ src/pages/HardwarePage.js
@@ -1,7 +1,25 @@
+import React, {useState, useEffect} from 'react';
 import "./HardwarePage.css";
 import HardwareList from "../components/HardwareList";
 
 function HardwarePage(){
+
+    const [capacity, setCapacity] = useState([])
+
+    useEffect(()=>{
+        fetch('/hwcapacity', {
+            headers : { 
+              'Content-Type': 'application/json',
+              'Accept': 'application/json'
+             }
+      
+          }).then(response =>{
+            if(response.ok){
+                return response.json()
+            }
+        }).then(data => console.log(data))
+    },[])
+
     return (
         <div>
             <div>Hardware page</div>
