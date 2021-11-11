bc4b998a17c83576932d83fa4b9a06a8e4c119ed
MandrewWhit <andrew.whitaker987@gmail.com>
2021-11-10 18:50:36 -0600

    sign up frontend

--- src/App.js
+++ src/App.js
@@ -1,30 +1,65 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 import NavBar from "./components/navBar";
 import Landing from "./pages/Landing";
 import DatasetPage from "./pages/DatasetPage";
 import HardwarePage from './pages/HardwarePage';
+import {Card, CardHeader, Button, CardContent, TextField} from "@mui/material";
 
 
 function App() {
-  return (
-        <>
-            <Router>
-                <NavBar/>
-                <div id="page-container">
 
-                </div>
-                <Switch>
-                    <Route path="/" exact component={Landing} />
-                    <Route path="/datasets" exact component={DatasetPage}/>
-                    <Route path="/hardware" exact component={HardwarePage}/>
-                </Switch>
-            
-            </Router>
-            
+    const [signin, setSignin] = useState(false);
 
-        </>
-  );
+    if(!signin){
+        return (
+            <div>
+                <Card className="sign-in-dialog">
+                    <CardContent>
+                        <h4>Already have an account?</h4>
+                        <TextField className="username-password" id="standard-basic" label="Username" variant="standard" />
+                        <TextField className="username-password" id="standard-basic" label="Password" variant="standard" />
+                        <div id="button-wrapper">
+                            <Button id="login-button" variant="outlined">Sign In</Button>
+                        </div>
+                        <h4>New User?</h4>
+                        <TextField className="username-password" id="standard-basic" label="New Username" variant="standard" />
+                        <TextField className="username-password" id="standard-basic" label="New Password" variant="standard" />
+                        <div id="button-wrapper">
+                            <Button id="login-button" variant="outlined">Sign Up</Button>
+                        </div>
+                    </CardContent>
+                </Card>
+                <Button id="login-button" variant="contained" onClick={() => {
+                    setSignin(true);
+                }}>Development Signin</Button>
+            </div>
+        );
+    }else{
+        return (
+            <>
+                <Router>
+                    <NavBar/>
+                    <div id="page-container">
+
+                    </div>
+                    <Switch>
+                        <Route path="/" exact component={Landing} />
+                        <Route path="/datasets">
+                            <DatasetPage/>
+                        </Route>
+                        <Route path="/hardware" exact component={HardwarePage}/>
+                    </Switch>
+                
+                </Router>
+                
+
+            </>
+        );
+    }
+
+    
 }
 
 export default App;
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -5,22 +5,11 @@ import React, { useState, useEffect } from 'react';
 
 function Landing(){
 
-    const [color, setColor] = useState("green");
 
     return (
-        <div>
-            <Card className="sign-in-dialog">
-                <CardContent>
-                    <h4>Already have an account?</h4>
-                    <TextField className="username-password" id="standard-basic" label="Username" variant="standard" />
-                    <TextField className="username-password" id="standard-basic" label="Password" variant="standard" />
-                    <h4>New User?</h4>
-                    <TextField className="username-password" id="standard-basic" label="New Username" variant="standard" />
-                    <TextField className="username-password" id="standard-basic" label="New Password" variant="standard" />
-                </CardContent>
-            </Card>
-        </div>
+        <p>Landing</p>
     );
+    
 }
 
 
